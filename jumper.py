e4adb91676cab284c6c65818c928fa913ad623e2
dANk <serg.miass340@gmail.com>
2021-01-15 00:24:36 +0500

    Sounds

--- jumper.py
+++ jumper.py
@@ -2,13 +2,25 @@ import pygame
 import pygame_gui
 import sys
 import os
+import csv, json
 from random import randrange, getrandbits, uniform
 
 WIDTH, HEIGHT = 1000, 500
 GRAVITY = 300
 
+MENU_MUSIC_VOLUME = 0.2
+GAME_MUSIC_VOLUME = 0.25
+PAUSE_MUSIC_VOLUME = 0.2
+PLAYER_LANDING_VOLUME = 0.8
+PLAYER_PUSH_VOLUME = 0.4
+PLAYER_DIE_VOLUME = 1
+ENEMY_LANDING_VOLUME = 0.5
+ENEMY_DIE_VOLUME = 0.6
+BOOM_VOLUME = 0.4
+
 GOD_MODE = 0
 SHOW_FPS_IN_GAME = 1
+
 
 def load_image(name, size=None, colorkey=None):
     fullname = os.path.join('data', name)
@@ -32,6 +44,14 @@ def load_image(name, size=None, colorkey=None):
                                                  (prev_w * size[1] // prev_h,
                                                   size[1]))
     return image
+
+
+def load_sound(name):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл звука '{fullname}' не найден")
+        sys.exit()
+    return pygame.mixer.Sound(fullname)
 
 
 class Camera:
@@ -118,6 +138,9 @@ class Sprite(pygame.sprite.Sprite):
     def is_playing(self):
         return bool(self.anim_name)
 
+    def on_map(self):
+        return 0 < self.rect.right < game.width + self.rect.width
+
     def destruct(self):
         super().kill()
 
@@ -171,6 +194,10 @@ class Player(Sprite):
         self.pos = list(pos)
         self.move_pos = list(pos)
 
+        self.push_sound = push_sound
+        self.landing_sound = landing_sound
+        self.die_sound = player_die_sound
+
         self.level = 0
         self.last_level = None
 
@@ -191,6 +218,8 @@ class Player(Sprite):
         self.push_phase += time
         self.jump_phase += time
         if push and not self.death:
+            if self.on_map():
+                self.push_sound.play()
             self.push_phase = 0
             self.in_pushing = True
             self.move_pos[0] = self.pos[0]
@@ -210,7 +239,9 @@ class Player(Sprite):
                 self.jump_phase = 0
                 self.move_pos[1] = platform.rect.topleft[1]
                 if not self.is_playing() and self.prev_name != "landing":
-                    self.start_anim("landing", 0.01)
+                    if self.on_map():
+                        self.start_anim("landing", 0.01)
+                    self.landing_sound.play()
                     self.is_jump = True
             elif self.rect.right > platform.rect.right:
                 self.pos[0] += 2
@@ -245,6 +276,7 @@ class Player(Sprite):
         if self.death:
             if self.anim_name != "die" and self.prev_name != "die":
                 self.start_anim("die", 0.05)
+                self.die_sound.play()
                 self.jump_phase = 0
                 self.jump_speed = 0
                 self.move_pos[1] = self.pos[1]
@@ -266,6 +298,9 @@ class Enemy(Sprite):
         self.rect.bottomleft = pos
         self.pos = list(pos)
         self.move_pos = list(pos)
+
+        self.landing_sound = landing_sound
+        self.die_sound = enemy_die_sound
 
         self.is_jump = True
         self.jump_phase = 0
@@ -291,6 +326,8 @@ class Enemy(Sprite):
                 self.move_pos[1] = platform.rect.topleft[1]
                 if not self.is_playing() and self.prev_name != "landing":
                     self.start_anim("landing", 0.01)
+                    if self.on_map():
+                        self.landing_sound.play()
                     self.is_jump = True
 
         if self.is_jump:
@@ -323,6 +360,8 @@ class Enemy(Sprite):
         if self.death:
             if self.anim_name != "die" and self.prev_name != "die":
                 self.start_anim("die", 0.05)
+                if self.on_map():
+                    self.die_sound.play()
                 self.jump_phase = 0
                 self.jump_speed = 0
                 self.move_pos[1] = self.pos[1]
@@ -343,6 +382,8 @@ class Bomb(Sprite):
         self.rect.bottom = pos[1]
         self.radius = 200
 
+        self.boom_sound = boom_sound
+
         self.player = None
 
     def update(self, time):
@@ -356,6 +397,7 @@ class Bomb(Sprite):
                 if pygame.sprite.collide_circle(self, entity):
                     entity.kill()
             self.start_anim("boom", 0.15)
+            self.boom_sound.play()
         if self.prev_name == "boom":
             self.kill()
 
@@ -408,17 +450,140 @@ class Start(Form):
                                                         text='Exit',
                                                         manager=self.manager)
 
+        self.music = menu_music
+        self.channel = None
+
     def main(self, events, timer):
         time_delta = timer.tick() / 1000.0
+        if not self.channel:
+            self.channel = self.music.play(-1)
         for event in events:
             if (event.type == pygame.USEREVENT and
                     event.user_type == pygame_gui.UI_BUTTON_PRESSED):
                 if event.ui_element == self.exit_button:
                     sys.exit()
                 if event.ui_element == self.play_button:
+                    self.channel = self.music.stop()
                     game.restart_game()
                     self.is_visible = False
                     game.is_visible = True
+                if event.ui_element == self.settings_button:
+                    self.is_visible = False
+                    settings_form.prev_form = self
+                    settings_form.is_visible = True
+
+            self.manager.process_events(event)
+
+        self.manager.update(time_delta)
+
+        self.screen.blit(self.background, (0, 0))
+        self.manager.draw_ui(self.screen)
+
+        pygame.display.update()
+
+
+class Settings(Form):
+    def __init__(self, screen):
+        super().__init__(screen)
+
+        self.prev_form = None
+        self.load_settings()
+
+        self.manager = pygame_gui.UIManager((self.width, self.height))
+
+        self.background = menu_bg_image
+        rect = pygame.Rect((0, 0), (300, 30))
+
+        rect.center = self.width // 2, self.height // 2 - (rect.height - 5) * 5
+        pygame_gui.elements.UILabel(relative_rect=rect,
+                                    text='Master Volume',
+                                    manager=self.manager)
+        rect.centery += rect.height + 5
+        self.master_volume = pygame_gui.elements.UIHorizontalSlider(relative_rect=rect,
+                                                                    start_value=self.settings["master_vol"],
+                                                                    value_range=(0.0, 1.0),
+                                                                    manager=self.manager)
+        rect.centery += (rect.height + 5) * 2
+        pygame_gui.elements.UILabel(relative_rect=rect,
+                                    text='Music Volume',
+                                    manager=self.manager)
+        rect.centery += rect.height + 5
+        self.music_volume = pygame_gui.elements.UIHorizontalSlider(relative_rect=rect,
+                                                                   start_value=self.settings["music_vol"],
+                                                                   value_range=(0.0, 1.0),
+                                                                   manager=self.manager)
+        rect.centery += rect.height + 5
+        pygame_gui.elements.UILabel(relative_rect=rect,
+                                    text='Effects Volume',
+                                    manager=self.manager)
+        rect.centery += rect.height + 5
+        self.effects_volume = pygame_gui.elements.UIHorizontalSlider(relative_rect=rect,
+                                                                     start_value=self.settings["effects_vol"],
+                                                                     value_range=(0.0, 1.0),
+                                                                     manager=self.manager)
+        rect.centery += rect.height + 5
+        rect.width //= 2
+        rect.centerx += rect.width + 2.5
+        self.exit_button = pygame_gui.elements.UIButton(relative_rect=rect,
+                                                        text='ok',
+                                                        manager=self.manager)
+
+    def load_settings(self):
+        self.settings = json.loads(open("settings.json").read())
+        self.set_volumes()
+
+    def update_settings(self):
+        open("settings.json", "w").write(json.dumps(self.settings))
+
+    def set_volumes(self):
+        start_form.music.set_volume(MENU_MUSIC_VOLUME *
+                                    self.settings["master_vol"] *
+                                    self.settings["music_vol"])
+        game.music.set_volume(GAME_MUSIC_VOLUME *
+                              self.settings["master_vol"] *
+                              self.settings["music_vol"])
+        pause.music.set_volume(PAUSE_MUSIC_VOLUME *
+                               self.settings["master_vol"] *
+                               self.settings["music_vol"])
+
+        player = game.player
+        if player:
+            player.push_sound.set_volume(PLAYER_PUSH_VOLUME *
+                                         self.settings["master_vol"] *
+                                         self.settings["effects_vol"])
+            player.landing_sound.set_volume(PLAYER_LANDING_VOLUME *
+                                            self.settings["master_vol"] *
+                                            self.settings["effects_vol"])
+            player.die_sound.set_volume(PLAYER_DIE_VOLUME *
+                                        self.settings["master_vol"] *
+                                        self.settings["effects_vol"])
+        for enemy in game.enemies_group.sprites():
+            enemy.landing_sound.set_volume(ENEMY_LANDING_VOLUME *
+                                           self.settings["master_vol"] *
+                                           self.settings["effects_vol"])
+            enemy.die_sound.set_volume(ENEMY_DIE_VOLUME *
+                                       self.settings["master_vol"] *
+                                       self.settings["effects_vol"])
+        for bomb in game.bombs_group.sprites():
+            bomb.boom_sound.set_volume(BOOM_VOLUME *
+                                       self.settings["master_vol"] *
+                                       self.settings["effects_vol"])
+
+    def main(self, events, timer):
+        time_delta = timer.tick() / 1000.0
+
+        for event in events:
+            if event.type == pygame.USEREVENT:
+                if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == self.exit_button:
+                        self.update_settings()
+                        self.is_visible = False
+                        self.prev_form.is_visible = True
+                if event.user_type == pygame_gui.UI_HORIZONTAL_SLIDER_MOVED:
+                    self.settings["master_vol"] = self.master_volume.get_current_value()
+                    self.settings["music_vol"] = self.music_volume.get_current_value()
+                    self.settings["effects_vol"] = self.effects_volume.get_current_value()
+                    self.set_volumes()
 
             self.manager.process_events(event)
 
@@ -449,6 +614,11 @@ class Game(Form):
         self.bombs_group = pygame.sprite.Group()
         self.platforms_group = pygame.sprite.Group()
 
+        self.player = None
+
+        self.music = game_music
+        self.music.set_volume(GAME_MUSIC_VOLUME)
+
     def restart_game(self):
         for sprite in self.all_sprites.sprites():
             sprite.destruct()
@@ -470,8 +640,8 @@ class Game(Form):
 
         self.time_text = Text("0", (0, 0), 50, "green", bg_color=(0, 0, 0, 190))
         self.level_text = Text("0", (self.width, 0), 50, "red", bg_color=(0, 0, 0, 190), align_left=False)
-        self.fps_text = Text("0", (self.width, 480), 20, ("white"), bg_color=(0, 0, 0, 255), align_left=False,
-                        padding=1)
+        self.fps_text = Text("0", (self.width, 480), 20, "white", bg_color=(0, 0, 0, 255), align_left=False,
+                             padding=1)
         if not SHOW_FPS_IN_GAME:
             self.fps_text.kill()
         self.game_over = False
@@ -479,10 +649,11 @@ class Game(Form):
         self.end_phase = 0
         self.round_time = 0
 
+        self.music.stop()
+        self.channel = self.music.play(-1)
 
     def main(self, events, timer):
         time = timer.tick() / 1000.0
-
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -494,6 +665,8 @@ class Game(Form):
                     if not self.player.death:
                         self.on_pause = not self.on_pause
                         pause.is_visible = self.on_pause
+                        if not self.on_pause:
+                            pause.channel = pause.music.stop()
 
         self.sky_group.draw(self.screen)
         self.bg_group.draw(self.screen)
@@ -524,7 +697,10 @@ class Game(Form):
         self.texts_group.draw(self.screen)
 
         if self.on_pause:
+            self.channel.pause()
             return False
+
+        self.channel.unpause()
 
         self.round_time += time if not self.player.death else 0
 
@@ -585,7 +761,7 @@ class Pause(Form):
 
         self.manager = pygame_gui.UIManager((self.width, self.height))
 
-        self.background = pygame.Surface((self.width, self.height), 
+        self.background = pygame.Surface((self.width, self.height),
                                          pygame.SRCALPHA)
         self.background.fill((0, 0, 0, 150))
         buttons_rect = pygame.Rect((0, 0), (300, 50))
@@ -609,18 +785,30 @@ class Pause(Form):
                                                         text='Exit',
                                                         manager=self.manager)
 
+        self.music = pause_music
+        self.channel = None
+
     def main(self, events, timer):
         time_delta = timer.tick() / 1000.0
+
+        if not self.channel:
+            self.channel = self.music.play(-1)
         for event in events:
             if (event.type == pygame.USEREVENT and
                     event.user_type == pygame_gui.UI_BUTTON_PRESSED):
                 if event.ui_element == self.exit_button:
                     self.is_visible = False
                     game.is_visible = False
-                    start_window.is_visible = True
+                    start_form.is_visible = True
+                    self.channel = self.music.stop()
                 if event.ui_element == self.resume_button:
+                    self.channel = self.music.stop()
                     self.is_visible = False
                     game.on_pause = False
+                if event.ui_element == self.settings_button:
+                    self.is_visible = False
+                    settings_form.prev_form = self
+                    settings_form.is_visible = True
 
             self.manager.process_events(event)
 
@@ -633,6 +821,7 @@ class Pause(Form):
 
 
 pygame.init()
+pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 timer = pygame.time.Clock()
 
@@ -654,20 +843,32 @@ enemy_die_sheet = load_image("enemy_die.png")
 fire_sheet = load_image("fire.png")
 boom_sheet = load_image("boom.png")
 
-start_window = Start(screen)
-game = Game(screen)
-pause = Pause(screen)
+menu_music = load_sound("menu.mp3")
+game_music = load_sound("game.mp3")
+pause_music = load_sound("pause.mp3")
+landing_sound = load_sound("landing.wav")
+push_sound = load_sound("push.wav")
+boom_sound = load_sound("boom.wav")
+player_die_sound = load_sound("player_die.ogg")
+enemy_die_sound = load_sound("enemy_die.wav")
 
 if __name__ == '__main__':
+    start_form = Start(screen)
+    game = Game(screen)
+    pause = Pause(screen)
+    settings_form = Settings(screen)
+
     running = True
     while running:
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 running = False
-        
-        if start_window.is_visible:
-            start_window.main(events, timer)
+
+        if settings_form.is_visible:
+            settings_form.main(events, timer)
+        if start_form.is_visible:
+            start_form.main(events, timer)
         if game.is_visible:
             game.main(events, timer)
         if pause.is_visible:
